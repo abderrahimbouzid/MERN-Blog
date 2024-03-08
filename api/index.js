9a8f80535cdcf7a2324de29c4c020af6ff33ce79
ABDERRAHIM BOUZID <63477841+abderrahimbouzid@users.noreply.github.com>
2024-03-09 00:13:44 +0100

    i create a run server

---
+++ api/index.js
@@ -0,0 +1,7 @@
+import Express from 'express';
+
+const app = Express()
+
+app.listen(3000,()=>{
+    console.log("listening to port 3000!!")
+})
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>App</h1>
+    
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+    </BrowserRouter>
   )
 }

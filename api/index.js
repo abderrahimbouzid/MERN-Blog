1595efb08fab78219c1c46423bf9074c0176c2c4
ABDERRAHIM BOUZID <63477841+abderrahimbouzid@users.noreply.github.com>
2024-03-09 14:29:58 +0100

    i connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import Express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+console.log("Database is connected")
+}).catch((err)=>{
+    console.log(`this is the err ${err}`)
+})
 
 const app = Express()
 

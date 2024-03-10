84cf4946b3a0a7c29f7072c2f30823ffec90a79a
ABDERRAHIM BOUZID <63477841+abderrahimbouzid@users.noreply.github.com>
2024-03-11 00:54:02 +0100

    create signup ui

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,9 @@
+import { Button, Label, TextInput } from 'flowbite-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <div>Sign in</div>
   )
 }
